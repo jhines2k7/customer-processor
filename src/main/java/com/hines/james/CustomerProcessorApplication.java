1ac5ec1dfb621398f1c658976437cfe2f24ee298
James Hines <jameshines10@gmail.com>
2017-04-05 07:29:30 -0400

    Initial commit

---
+++ src/main/java/com/hines/james/CustomerProcessorApplication.java
@@ -0,0 +1,12 @@
+package com.hines.james;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerProcessorApplication.class, args);
+	}
+}
